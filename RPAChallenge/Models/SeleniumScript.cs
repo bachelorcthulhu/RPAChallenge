35cc3a9e3154cab5610286896b9113d5edc6d6a5
Vladislav Fedorov <bachelor.vladislav.fedorov@gmail.com>
2023-06-11 16:34:11 +0700

    base seleniumScript created

--- RPAChallenge/Models/SeleniumScript.cs
+++ RPAChallenge/Models/SeleniumScript.cs
@@ -14,34 +14,86 @@ public enum Browser
 
 public class SeleniumScript
 {
-    private string Url { get; }
-    private IWebDriver Driver { get; }
-    private List<Employee> Employees { get; }
+    private readonly string _url;
+    private readonly IWebDriver _driver;
+    private List<Employee> _employees;
+    private readonly List<string> _fieldsToFill;
+    /// <summary>
+    /// Единственные вшитые данные, количество раундов на RPA
+    /// </summary>
+    private readonly int _numberOfRounds = 10;
 
-    public SeleniumScript(string url, Browser browser, List<Employee> employees)
+    public SeleniumScript(string url, Browser browser, List<Employee> employees, List<string> fieldsToFill)
     {
-        Url = url;
-        Employees = new List<Employee>(employees);
+        _url = url;
+        _employees = new List<Employee>(employees);
+        _fieldsToFill = new List<string>(fieldsToFill);
+
+        this._driver = browser switch
+        {
+            Browser.Firefox => new FirefoxDriver(),
+            Browser.Chrome => new ChromeDriver(),
+            Browser.Edge => new EdgeDriver(),
+            _ => new EdgeDriver()
+        };
+    }
+    
+    public SeleniumScript(string url, Browser browser, List<string> fieldsToFill)
+    {
+        _url = url;
+        _employees = new List<Employee>();
+        _fieldsToFill = new List<string>(fieldsToFill);
 
         switch (browser)
         {
             case Browser.Firefox:
-                this.Driver = new FirefoxDriver();
+                this._driver = new FirefoxDriver();
                 break;
             case Browser.Chrome:
-                this.Driver = new ChromeDriver();
+                this._driver = new ChromeDriver();
                 break;
             case Browser.Edge:
-                this.Driver = new EdgeDriver();
+                this._driver = new EdgeDriver();
                 break;
             default:
-                this.Driver = new EdgeDriver();
+                this._driver = new EdgeDriver();
                 break;
+        }
+        
+        _driver.Navigate().GoToUrl(_url);
+    }
+
+    private void FillField(string fieldName, string inputData, IList<IWebElement> inputs)
+    {
+        foreach (IWebElement input in inputs)
+        {
+            if (input.GetAttribute("outerHTML").Contains(fieldName))
+            {
+                input.SendKeys(inputData);
+                return;
+            }
         }
     }
 
-    public void FillForm()
+    private void FillForm()
     {
+        IList<IWebElement> inputs = _driver.FindElements(By.TagName("input"));
+
+        foreach (var field in _fieldsToFill)
+        {
+            FillField(field, "sad", inputs);
+        }
         
+        _driver.FindElement(By.CssSelector("input.btn")).Click();
+    }
+
+    public void FillForms()
+    {
+        _driver.FindElement(By.CssSelector("button.waves-effect")).Click();
+        
+        for (var i = 0; i < _numberOfRounds; i++)
+        {
+            FillForm();
+        }
     }
 }
--- RPAChallenge/Program.cs
+++ RPAChallenge/Program.cs
@@ -1,3 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using RPAChallenge.Models;
+
 Console.WriteLine("Hello, World!");
+
+List<string> fields = new List<string>
+{
+    "labelFirstName", "labelLastName","labelCompanyName","labelRole",
+    "labelAddress", "labelEmail","labelPhone"
+};
+
+SeleniumScript RPAScript = new SeleniumScript("https://www.rpachallenge.com/", Browser.Firefox, fields);
+RPAScript.FillForms();
+Console.WriteLine();
